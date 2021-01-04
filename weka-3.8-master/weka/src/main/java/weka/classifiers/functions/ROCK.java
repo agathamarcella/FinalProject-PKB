4721cc272276954d18d629a8f39f77730bedbb26
avocado23 <56811810+avocado23@users.noreply.github.com>
2021-01-05 00:00:34 +0700

    Update ROCK.java

--- weka-3.8-master/weka/src/main/java/weka/classifiers/functions/ROCK.java
+++ weka-3.8-master/weka/src/main/java/weka/classifiers/functions/ROCK.java
@@ -6,30 +6,245 @@ import weka.core.*;
 import java.util.*;
 
 public class ROCK extends AbstractClassifier implements OptionHandler {
-/* @Override
-    public Distance calculateDistance(Collection<Distance> distances) {
-        double sum = 0;
-        double result;
 
-        for (Distance dist : distances) {
-            sum += dist.getDistance();
+    public class Cluster {
+        public void main {
+            elements = this.elements();
+            for { this.add(cluster);
         }
-        if (distances.size() > 0) {
-            result = sum / distances.size();
-        } else {
-            result = 0.0;
+    }
+
+        public void add(this, c) {
+            if {
+            isinstance(c, Cluster);
+            {
+                for {
+                e in c.get_elements();
+            }
+                this.elements.add(e);
+            }
+                else
+            {
+                this.elements.add(c);
+
+            }
         }
-        return  new Distance(result);
-    }
-*/
+            public void contains(this, e){
 
-/* Calculate the similarity between two clusters according to the goodness measure formula
- g(Ci, Cj) = [link(Ci, Cj)] / [(ni + nj)^(1+2f(theta)) - (ni)^(1+2f(theta)) */
-    private class CalcGoodness() {
-
-        private int Link;
-        private int Cluster;
-        private int i;
-        private int j;
+                if (e in) {
+                    this.elements;
+                    return True;
+                }
+                else {
+                    return False;
+                    }
+                }
+            }
+    
+    get_elements(this){
+        return this.elements;
     }
-}
+
+        inspect(this){
+                for
+        e in
+        this.elements;
+        print '\t\t'+e.inspect();
+
+        size(this){
+                        return len(this.elements);
+                }
+
+    }
+
+    class ClusterSet {
+        public void {
+            this.all_clusters = set();
+        }
+        
+        public void add(this, c) {
+            if isinstance(c, Cluster) {
+                this.all_clusters.add(c);
+            }
+        }
+    
+        public void remove(this, c) {
+            this.all_clusters.remove(c);
+        }
+        
+        public size(this) {
+            return len(this.all_clusters);
+        }
+
+        public get_all_clusters(this) {
+            return this.all_clusters;
+        }
+    }
+
+    class DataPoint {
+        (this,label,attr)
+                this.label =label;
+        this.attr =attr;
+
+        public get_text_attr_values(this) {
+            return this.attr;
+        }
+
+        public inspect(this) {
+            content = "{%s:%s}" % (this.label, this.attr);
+            return content;
+        }
+    }
+
+    class Dendrogram {
+        public void (this, level_label_name) {
+            this.level_label_name = level_label_name;
+            this.next_level = 0;
+            this.entry_map =[];
+            this.level_labels =[];
+        }
+
+        public void add_level(this, label, clusters) {
+            cluster_set = ClusterSet();
+            for cluster in clusters {
+                cluster_set.add(copy.copy(cluster));
+            }
+            level = this.next_level
+            this.entry_map.append(cluster_set);
+            this.level_labels.append(label);
+        }
+
+        public  this.next_level+=1 {
+            return level;
+        }
+        public void inspect_all(this):
+                for
+        level in
+
+        range(len(this.level_labels)):
+                this.inspect(level)
+
+        public void inspect(this, level):
+        cluster_num=1
+                if this.entry_map[level].
+
+        size()>0:
+        print "=================================================================="
+        print "Clutser for level %s; %s=%s; Number of clusters:%s"%(level,this.level_label_name,this.level_labels[level],this.entry_map[level].
+
+        size())
+                for
+        cluster in
+        this.entry_map[level].
+
+        get_all_clusters();
+                if cluster.size()>0;
+        print '\tCluster No.%s:contains %s elements'%(cluster_num,cluster.size())
+        cluster_num+=1
+                # cluster.inspect()
+
+        public void inspect_top(this):
+                this.inspect(this.next_level-1)
+    }
+
+    class JaccardCoefficient {
+        public void similarity(this, x, y):
+                if
+
+        len(x)==0
+
+        or len(y)==0:
+                return 0.0
+                else:
+        set_x =
+
+        set(x)
+
+        set_y =
+
+        set(y)
+
+        union_xy =set_x |
+        set_y
+                intersection_xy = set_x & set_y
+			return
+
+        float(len(intersection_xy))/
+
+        float(len(union_xy))
+    }
+
+    class MergeGoodnessMeasure {
+        public void(this,th) {
+            this.link_threshold = th;
+            this.p = 1.0 + 2.0 * this.f(this.link_threshold);
+        }
+
+        public g(this,n_links, nx, ny) {
+            a = (nx + ny) **this.p;
+            b = nx **this.p;
+            c = ny **this.p;
+            return float(n_links) / (a - b - c);
+        }
+
+        public f(this,th) {
+            return (1.0 - th) / (1.0 + th);
+        }
+    }
+
+    class main {
+        int a[] = {1,0,1,0,1};
+        int b[] = {1,0,1,0,0};
+        int c[] = {0,0,0,1,0};
+        int d[] = {1,1,1,1,1};
+        int e[] = {0,1,1,0,0};
+
+        dps = []
+                dps.append(DataPoint('a',a));
+                dps.append(DataPoint('b',b));
+                dps.append(DataPoint('c',c));
+                dps.append(DataPoint('d',d));
+                dps.append(DataPoint('e',e));
+
+        rock = RockAlgorithm(dps,3,0.2);
+        dnd = rock.cluster();
+
+                dnd.inspect_all();
+    }
+
+    class RockAlgorithm{
+        RockAlgorithm {
+        public void(this, points, k, th){
+                this.points = points;
+                this.k = k;
+                this.th = th;
+                similarity_measure = JaccardCoefficient();
+                this.link_matrix = LinkMatrix(points, similarity_measure, th);
+            }
+
+            public void cluster (this) {
+            initial_clusters =[]
+            for point in this.points;
+            initial_clusters.append(Cluster(point))
+
+            dnd = Dendrogram("Goodness")
+        }
+
+    dnd.add_level(float('inf'),initial_clusters);
+
+        goodness_measure = MergeGoodnessMeasure(this.th)
+
+        all_clusters = RockClusters(initial_clusters,this.link_matrix,goodness_measure)
+
+        n_clusters = all_clusters.size();
+                while n_clusters>this.k;
+        n_clusters_before_merge = n_clusters;
+                g = all_clusters.merge_best_candidates();
+        n_clusters = all_clusters.size();
+                if {
+                n_clusters == n_clusters_before_merge;
+                break dnd.add_level(str(g), all_clusters.get_all_clusters());
+
+                return dnd;
+            }
+    }
